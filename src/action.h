331baec893da3aea06d1374b886c09f3de5639e4
Deniz Celebi <5246652+derDeno@users.noreply.github.com>
2024-10-20 00:56:07 +0200

    wip

--- src/action.h
+++ src/action.h
@@ -1,68 +1,51 @@
+#include <Arduino.h>
 
-void actionPinControl(int pin, bool turn_on, ) {
+// Pin Control action. Perform action defined in mapping
+void actionPinControl(String pin, String control, bool invert = false, int invert_delay = 0, int flash_timout = 0, int flash_count = 0) {
 
+    int outputPin = outputToPin(pin);
+
+    // first check if option is set to flash
+    if(control == "flash") {
+        for(int i = 0; i < flash_count; i++) {
+            digitalWrite(outputPin, HIGH);
+            delay(flash_timout * 1000);
+            digitalWrite(outputPin, LOW);
+            delay(flash_timout * 1000);
+        }
+    } else if (control == "on") {
+        digitalWrite(outputPin, HIGH);
+        if(invert) {
+            delay(invert_delay * 1000);
+            digitalWrite(outputPin, LOW);
+        }
+    } else if (control == "off") {
+        digitalWrite(outputPin, LOW);
+        if(invert) {
+            delay(invert_delay * 1000);
+            digitalWrite(outputPin, HIGH);
+        }
+    }
 }
 
+// RGB Control action. Perform action defined in mapping
+
+
+
 // convert output names to actual pin numbers
-int outputToPin(char *output) {
-    if(output == "") {
-
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
-    }else if (output == "") {
-        /* code */
+int outputToPin(String output) {
+    if (output == "analog-r") {
+        return 0;
+    }else if (output == "analog-g") {
+        return 0;
+    }else if (output == "analog-b") {
+        return 0;
+    }else if (output == "analog-ww") {
+        return 0;
+    }else if (output == "analog-cw") {
+        return 0;
     }else {
-        return -1,
+        // default
+        return output.toInt();
     }
-    
-
-    return 0;
 }
