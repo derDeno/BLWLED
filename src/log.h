ae4a07a78b0f54c86811ba07648387ae1c9864b9
Deniz Celebi <5246652+derDeno@users.noreply.github.com>
2024-12-13 02:28:44 +0100

    max log file size reduced to 50kb

--- src/log.h
+++ src/log.h
@@ -2,83 +2,83 @@
 #include <LittleFS.h>
 
 struct tm timeinfo;
-const size_t MAX_LOG_FILE_SIZE = 200 * 1024;  // 200 KB
+const size_t MAX_LOG_FILE_SIZE = 50 * 1024;  // 50 KB
 extern AppConfig appConfig;
 
 void checkLogFileSize(const char* fileName) {
-  File logFile = LittleFS.open(fileName, "r");
-  if (!logFile) {
-    Serial.println("Failed to open log file for reading");
-    return;
-  }
-
-  // Check the current file size
-  size_t fileSize = logFile.size();
-  logFile.close();
-
-  // If the file exceeds the max size, trim the file
-  if (fileSize > MAX_LOG_FILE_SIZE) {
-    // Open the log file again for reading to trim data
-    logFile = LittleFS.open(fileName, "r");
-    String newContent;
-    size_t bytesToTrim = fileSize - MAX_LOG_FILE_SIZE;
-
-    // Skip older lines until the file is under size limit
-    size_t currentSize = 0;
-    while (logFile.available()) {
-      String line = logFile.readStringUntil('\n');
-      currentSize += line.length() + 1;  // +1 for newline character
-
-      // Once we have skipped enough lines to be under the limit, start storing
-      if (currentSize > bytesToTrim) {
-        newContent += line + "\n";
-      }
+    File logFile = LittleFS.open(fileName, "r");
+    if (!logFile) {
+        Serial.println("Failed to open log file for reading");
+        return;
     }
 
+    // Check the current file size
+    size_t fileSize = logFile.size();
     logFile.close();
 
-    // Write the new trimmed content back to the file
-    logFile = LittleFS.open(fileName, "w");
-    if (logFile) {
-      logFile.print(newContent);
-      logFile.close();
-      Serial.println("Log file trimmed successfully");
-    } else {
-      Serial.println("Failed to open log file for writing");
+    // If the file exceeds the max size, trim the file
+    if (fileSize > MAX_LOG_FILE_SIZE) {
+        // Open the log file again for reading to trim data
+        logFile = LittleFS.open(fileName, "r");
+        String newContent;
+        size_t bytesToTrim = fileSize - MAX_LOG_FILE_SIZE;
+
+        // Skip older lines until the file is under size limit
+        size_t currentSize = 0;
+        while (logFile.available()) {
+            String line = logFile.readStringUntil('\n');
+            currentSize += line.length() + 1;  // +1 for newline character
+
+            // Once we have skipped enough lines to be under the limit, start storing
+            if (currentSize > bytesToTrim) {
+                newContent += line + "\n";
+            }
+        }
+
+        logFile.close();
+
+        // Write the new trimmed content back to the file
+        logFile = LittleFS.open(fileName, "w");
+        if (logFile) {
+            logFile.print(newContent);
+            logFile.close();
+            Serial.println("Log file trimmed successfully");
+        } else {
+            Serial.println("Failed to open log file for writing");
+        }
     }
-  }
 }
 
 void logger(String logData) {
-  getLocalTime(&timeinfo);
+    getLocalTime(&timeinfo);
 
-  char timeStringBuff[25];
-  strftime(timeStringBuff, sizeof(timeStringBuff), "[%Y-%m-%d %H:%M:%S]", &timeinfo);
-  String logMessage = String(timeStringBuff) + " - " + logData;
+    char timeStringBuff[25];
+    strftime(timeStringBuff, sizeof(timeStringBuff), "[%Y-%m-%d %H:%M:%S]", &timeinfo);
+    String logMessage = String(timeStringBuff) + " - " + logData;
 
-  Serial.println(logMessage);
-  delay(10);
+    Serial.println(logMessage);
+    delay(10);
 
-  // if debug logging is false quit
-  if(!appConfig.logging) {
-    return;
-  }
+    // if debug logging is false quit
+    if (!appConfig.logging) {
+        return;
+    }
 
-  // logging set to true so log to file
-  checkLogFileSize("/log.txt");
+    // logging set to true so log to file
+    checkLogFileSize("/log.txt");
 
-  File logFile = LittleFS.open("/log.txt", "a");
-  if (!logFile) {
-    Serial.println("Failed to open log file");
-    return;
-  }
+    File logFile = LittleFS.open("/log.txt", "a");
+    if (!logFile) {
+        Serial.println("Failed to open log file");
+        return;
+    }
 
-  logFile.println(logMessage);
-  logFile.close();
+    logFile.println(logMessage);
+    logFile.close();
 }
 
 void deleteLogFile() {
   if (LittleFS.exists("/log.txt")) {
-    LittleFS.remove("/log.txt");
+      LittleFS.remove("/log.txt");
   }
 }
